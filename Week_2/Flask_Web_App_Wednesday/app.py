d47128d4d5526ba516294b2313f0a65898512cf3
Lannon Khau <49844413+LannonTheCannon@users.noreply.github.com>
2025-04-23 20:54:10 -0700

    update cloudyberry

--- Week_2/Flask_Web_App_Wednesday/app.py
+++ Week_2/Flask_Web_App_Wednesday/app.py
@@ -2,19 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    name = request.form['name']
-    email = request.form['email']
-    message = request.form['message']
-
-    return render_template('feedback.html', username = name, message=message, email=email)
-
 @app.route('/greet', methods=['GET', "POST"])
 def greet():
     if request.method == "POST":
         name = request.form['name']
-        return render_template('user.html', username = name)
+        email = request.form['email']
+        message = request.form['message']
+        return render_template('feedback.html', username=name, message=message, email=email)
+
     return render_template('form.html')
 
 if __name__ == '__main__':
