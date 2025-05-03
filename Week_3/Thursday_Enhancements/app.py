10b29392ce1f86689ce485a5159f59657df88cbc
Lannon Khau <49844413+LannonTheCannon@users.noreply.github.com>
2025-05-03 12:48:13 -0700

    update

--- Week_3/Thursday_Enhancements/app.py
+++ Week_3/Thursday_Enhancements/app.py
@@ -65,6 +65,7 @@ def register():
             db.session.commit()
             flash('Registration successful. Please log in.')
             return redirect(url_for('login'))
+
     return render_template('auth.html', action='Register')
 
 @app.route('/login', methods=['GET','POST'])
