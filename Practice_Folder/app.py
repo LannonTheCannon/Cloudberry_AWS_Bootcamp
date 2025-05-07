0452505cb53be97e8139b2cceb2fdd4e681c2d20
Lannon Khau <49844413+LannonTheCannon@users.noreply.github.com>
2025-05-07 13:26:34 -0700

    Update app.py

--- Practice_Folder/app.py
+++ Practice_Folder/app.py
@@ -152,4 +152,4 @@ if __name__ == '__main__':
     # create auth tables if they don’t exist
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5002, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
