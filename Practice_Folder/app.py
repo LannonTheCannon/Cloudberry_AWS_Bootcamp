77b15d957cd4ff0e5dd766f39b0bd70529ba31af
Lannon Khau <49844413+LannonTheCannon@users.noreply.github.com>
2025-05-08 22:59:15 -0700

    Update app.py

--- Practice_Folder/app.py
+++ Practice_Folder/app.py
@@ -36,12 +36,14 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_REGION            = os.getenv('AWS_REGION')
 S3_BUCKET_NAME        = 'dataforge-uploader-bucket'
 
-s3_client = boto3.client(
-    's3',
-    aws_access_key_id     = AWS_ACCESS_KEY_ID,
-    aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
-    region_name           = AWS_REGION
-)
+# s3_client = boto3.client(
+#     's3',
+#     aws_access_key_id     = AWS_ACCESS_KEY_ID,
+#     aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
+#     region_name           = AWS_REGION
+# )
+
+s3_client = boto3.client("s3")
 
 # ─── MODELS ─────────────────────────────────────────────────────────────────────
 
