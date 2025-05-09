2d489720b5fb02a577a67f8d980160b909437179
Lannon Khau <49844413+LannonTheCannon@users.noreply.github.com>
2025-05-09 11:03:59 -0700

    Update app.py

--- Practice_Folder/app.py
+++ Practice_Folder/app.py
@@ -279,4 +279,6 @@ def delete_file(file_id):
 # ─── ENTRYPOINT ────────────────────────────────────────────────────────────────
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
