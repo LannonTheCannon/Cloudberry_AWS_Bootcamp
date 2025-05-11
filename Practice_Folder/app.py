6ba2a727f5998efea1ebfb2c82c37491a543efa7
Lannon Khau <49844413+LannonTheCannon@users.noreply.github.com>
2025-05-11 16:51:14 -0700

    Update app.py

--- Practice_Folder/app.py
+++ Practice_Folder/app.py
@@ -19,7 +19,7 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = 'SuperSecretKey'
 
-def get_db_secret(secret_name, region_name='us-east-2'):
+def get_db_secret(secret_name, region_name='us-east-1'):
     client = boto3.client('secretsmanager', region_name=region_name)
     get_secret_value_response = client.get_secret_value(SecretId=secret_name)
     secret = get_secret_value_response['SecretString']
