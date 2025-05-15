b5b5eb300698c6f92657de0ec55dea889e9686a1
Lannon Khau <49844413+LannonTheCannon@users.noreply.github.com>
2025-05-15 11:44:18 -0700

    changed host
    0.0.0.0

--- Practice_Folder/app.py
+++ Practice_Folder/app.py
@@ -362,4 +362,4 @@ def clean_file(file_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5001, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
