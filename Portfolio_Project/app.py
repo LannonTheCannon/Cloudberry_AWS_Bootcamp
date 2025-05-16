f08b0870dd22bab39b64153135aaaf8a07ffa1ba
Lannon Khau <49844413+LannonTheCannon@users.noreply.github.com>
2025-05-16 15:09:15 -0700

    Update app.py

--- Portfolio_Project/app.py
+++ Portfolio_Project/app.py
@@ -244,7 +244,8 @@ def home():
 def show_projects():
     return render_template('projects.html', projects=projects)
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
