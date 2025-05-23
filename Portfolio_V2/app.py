fb67c65b71befb49858f942139ed1592e491e3d7
Lannon Khau <49844413+LannonTheCannon@users.noreply.github.com>
2025-05-23 00:55:25 -0700

    Fix Typo

--- Portfolio_V2/app.py
+++ Portfolio_V2/app.py
@@ -357,11 +357,8 @@ def clean_file(file_id):
         df = pd.read_csv(BytesIO(s3_obj['Body'].read()))
 
         # Run AI pipeline
-<<<<<<< HEAD
+
         df_cleaned = run_clean_pipeline(df)
-=======
-        df_cleaned = run_cleaning_pipeline(df)
->>>>>>> 8c62c08 (Add AI cleaning button to dashboard)
 
         # Upload cleaned file
         buffer = BytesIO
